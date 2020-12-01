9dac66759a93e569e0ee3c762a34311ed3f237ef
kiyoshi-kk12 <ilvpotatowww@gmail.com>
2020-12-02 00:56:14 +0900

    問題はあるけどとりあえずjobは完成

--- app/jobs/get_horse_info_by_jra_job.rb
+++ app/jobs/get_horse_info_by_jra_job.rb
@@ -4,7 +4,7 @@ class GetHorseInfoByJraJob < ApplicationJob
   def perform(url)
     get_site_info = Mechanize.new.get(url)
     elements = get_site_info.search('tr')
-    base_date= url.slice("https://jra.jp/keiba/thisweek/".length..url.length).split("/")
+    base_date = url.slice("https://jra.jp/keiba/thisweek/".length..url.length).split("/")
     event_date = Date.new(base_date.first.to_i, base_date.second.slice(0..1).to_i, base_date.second.slice(2..3).to_i)
     elements.slice(1..elements.length).each do |ele|
       father = nil
@@ -12,48 +12,33 @@ class GetHorseInfoByJraJob < ApplicationJob
       ele.inner_text.split.select{ |i| (i =~ /父|母/) }.each do |i|
         array_string = i.split("")
         if array_string.first == "父" && array_string.second == "："
-          father = Father.create(name: array_string.slice(2..array_string.length).join)
+          father = Father.find_or_create_by(name: array_string.slice(2..array_string.length).join)
         elsif array_string.first == "母" && array_string.second == "："
-          mother = Mother.create(name: array_string.slice(2..array_string.length).join)
+          mother = Mother.find_or_create_by(name: array_string.slice(2..array_string.length).join)
         end
       end
-      horse = Horse.create(name: ele.inner_text.split.second, father: father, mother: mother)
+      # ele.children[5].children[1].children[1].children[0].text.split.firstでも馬の名前取れるっぽい
+      horse = Horse.find_or_create_by(name: ele.inner_text.split.second, father: father, mother: mother)
       past_result_params(ele, horse, event_date).each {|params| PastResult.create(params)}
     end
   end
 
   # 配列固定したけど、列によって違うためnameやclassで取ったほうが良い
+  # 名前の最後に"芝"か"ダ"が来たらアウト
   def past_result_params(ele, horse, event_date)
-    [
+    rankings = ele.inner_text.split.select{|i| i.include?("着")}
+    distances = ele.inner_text.split.select{|i| ["芝", "ダ"].include?(i.split("").last)}
+    times = ele.inner_text.split.select{|i| i.include?(":") && i.include?(".")}
+    params = rankings.map.with_index do |ranking, index|
       {
         horse: horse,
-        ranking: ele.inner_text.split[19].slice(0).to_i,
-        distance: ele.inner_text.split[24].gsub(/芝/, '').to_i,
+        ranking: ranking.slice(0).to_i,
+        distance: distances[index].gsub(/芝|ダ/, '').to_i,
         match_day: event_date,
-        time: calculation_time(ele.inner_text.split[26])
-      },
-      {
-        horse: horse,
-        ranking: ele.inner_text.split[35].slice(0).to_i,
-        distance: ele.inner_text.split[40].gsub(/芝/, '').to_i,
-        match_day: event_date,
-        time: calculation_time(ele.inner_text.split[42])
-      },
-      {
-        horse: horse,
-        ranking: ele.inner_text.split[51].slice(0).to_i,
-        distance: ele.inner_text.split[56].gsub(/芝/, '').to_i,
-        match_day: event_date,
-        time: calculation_time(ele.inner_text.split[58])
-      },
-      {
-        horse: horse,
-        ranking: ele.inner_text.split[67].slice(0).to_i,
-        distance: ele.inner_text.split[72].gsub(/芝/, '').to_i,
-        match_day: event_date,
-        time: calculation_time(ele.inner_text.split[74])
+        time: calculation_time(times[index])
       }
-    ]
+    end
+    params
   end
 
   def calculation_time(time)
