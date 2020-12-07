be84c4212b82c939c773d23e9ff9c37b58aa2394
kiyoshi-kk12 <ilvpotatowww@gmail.com>
2020-12-07 22:40:55 +0900

    rubocop導入&対応

--- app/jobs/get_horse_info_by_jra_job.rb
+++ app/jobs/get_horse_info_by_jra_job.rb
@@ -9,7 +9,7 @@ class GetHorseInfoByJraJob < ApplicationJob
     elements.slice(1..elements.length).each do |ele|
       father = nil
       mother = nil
-      ele.inner_text.split.select{ |i| (i =~ /父|母/) }.each do |i|
+      ele.inner_text.split.select { |i| (i =~ /父|母/) }.each do |i|
         array_string = i.split("")
         if array_string.first == "父" && array_string.second == "："
           father = Father.find_or_create_by(name: array_string.slice(2..array_string.length).join)
@@ -19,16 +19,16 @@ class GetHorseInfoByJraJob < ApplicationJob
       end
       # ele.children[5].children[1].children[1].children[0].text.split.firstでも馬の名前取れるっぽい
       horse = Horse.find_or_create_by(name: ele.inner_text.split.second, father: father, mother: mother)
-      past_result_params(ele, horse, event_date).each {|params| PastResult.create(params)}
+      past_result_params(ele, horse, event_date).each { |params| PastResult.create(params) }
     end
   end
 
   # 配列固定したけど、列によって違うためnameやclassで取ったほうが良い
   # 名前の最後に"芝"か"ダ"が来たらアウト
   def past_result_params(ele, horse, event_date)
-    rankings = ele.inner_text.split.select{|i| i.include?("着")}
-    distances = ele.inner_text.split.select{|i| ["芝", "ダ"].include?(i.split("").last)}
-    times = ele.inner_text.split.select{|i| i.include?(":") && i.include?(".")}
+    rankings = ele.inner_text.split.select { |i| i.include?("着") }
+    distances = ele.inner_text.split.select { |i| ["芝", "ダ"].include?(i.split("").last) }
+    times = ele.inner_text.split.select { |i| i.include?(":") && i.include?(".") }
     params = rankings.map.with_index do |ranking, index|
       {
         horse: horse,
