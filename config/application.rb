3f12d2cefd3a641d39fb075ab60304a57c6e1396
kiyoshi-kk12 <ilvpotatowww@gmail.com>
2020-12-25 23:02:16 +0900

    sidekiq追加

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module HorseRacingForecast
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.active_job.queue_adapter = :sidekiq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'horses#index'
   resources :horses, only: :show
   post '/access_site', to: "horses#access_site"
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: "/sidekiq"
 end
